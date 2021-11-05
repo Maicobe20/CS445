981a27025017b2045d0f3f4a68d4b437b749b994
Maicobe20 <mikeberhane2020@gmail.com>
2021-11-04 19:45:26 -0500

    Lab5 assignment solutions

--- Lab5/exercise01.js
+++ Lab5/exercise01.js
@@ -2,8 +2,8 @@
 const isPrime = num => {
   return new Promise((resolve,reject)=>{
        setTimeout(function(){ for(let i = 2, s = Math.sqrt(num); i <= s; i++)
-        if(num % i === 0) resolve({prime:false}); 
-    reject({prime: num > 1})}) ;
+        if(num % i === 0) reject({prime:false}); 
+    resolve({prime: num > 1})}) ;
     })
    
 }
